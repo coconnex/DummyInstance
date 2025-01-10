baf7ea909fe919356a939fcfda2dc52f2d6afd6f
coconnex <subodhs@cohezia.com>
2025-01-10 15:02:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
-This is Test file.
-This is implementation demo.
-This is implementation demo for VB123
-This is implementation demo for github
+<?php
 
-clarion test
-clarion test2
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+ini_set("log_errors", 1);
+ini_set("error_log", dirname(__FILE__)."/error_log");
+require_once './includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+//menu_rebuild();
+$return = menu_execute_active_handler();
+
+// Menu status constants are integers; page content is a string.
+if (is_int($return)) {
+  switch ($return) {
+    case MENU_NOT_FOUND:
+      drupal_not_found();
+      break;
+    case MENU_ACCESS_DENIED:
+      drupal_access_denied();
+      break;
+    case MENU_SITE_OFFLINE:
+      drupal_site_offline();
+      break;
+  }
+}
+elseif (isset($return)) {
+  // Print any value (including an empty string) except NULL or undefined:
+  print theme('page', $return);
+}
+
+drupal_page_footer();
