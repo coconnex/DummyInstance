acfc71e60c73d734a68db26e0861fc1c582af34a
Naman Guntiwar <namang@cohezia.com>
2025-02-14 18:21:38 +0530

    made changes manually

--- index.php
+++ index.php
@@ -2,14 +2,5 @@ This is Test file.
 This is implementation demo.
 
 new contrib --
-<<<<<<< Updated upstream
-Now we are transforming from VSS to git.
-
-
-
-
-
-
-
 
 Now we are transforming from VSS to git.
